a27885b9e5aab9670c809fa72dd16d7b8861657a
宅爷 <744561141@qq.com>
2026-05-20 13:55:49 +0800

    ⭐ 仓库首次上传

---
+++ data.js
@@ -0,0 +1,303 @@
+window.data = {
+  "暗极妈": {
+    "rows": [
+      {
+        "branches": [
+          {
+            "name": "开场",
+            "items": [
+              {
+                "title": "t1",
+                "awakening": true,
+                "content": "B1\nB2\nB3\nB2\nC1\nC3\nD1\nD3\nA"
+              },
+              {
+                "title": "t2",
+                "awakening": true,
+                "content": "B\nA1\nE4\nE3>A\nE2>D\nC1\nC3"
+              },
+              {
+                "title": "t3",
+                "awakening": true,
+                "content": "A2\n火解除"
+              },
+              {
+                "title": "t4",
+                "awakening": true,
+                "content": "C\nD2\nFC\n大药\nD2\n等待所有火消除后A"
+              }
+            ]
+          }
+        ],
+        "currentBranch": 0
+      },
+      {
+        "branches": [
+          {
+            "name": "火消除",
+            "items": [
+              {
+                "title": "t5",
+                "awakening": false,
+                "content": "A3\nF2>G\nD1\n等待90血后A"
+              }
+            ]
+          }
+        ],
+        "currentBranch": 0
+      },
+      {
+        "branches": [
+          {
+            "name": "90血以下",
+            "items": [
+              {
+                "title": "t6",
+                "awakening": false,
+                "content": "B\nA4\nG2\nC1\nC2\nC3"
+              },
+              {
+                "title": "T7",
+                "awakening": true,
+                "content": "开奥义直接打"
+              },
+              {
+                "title": "T8",
+                "awakening": false,
+                "content": "FC\n等待70血以下A"
+              }
+            ]
+          }
+        ],
+        "currentBranch": 0
+      },
+      {
+        "branches": [
+          {
+            "name": "红",
+            "items": [
+              {
+                "title": "红",
+                "awakening": true,
+                "content": "A\nA1\nD1\n等蓝条50了A"
+              }
+            ]
+          },
+          {
+            "name": "蓝",
+            "items": [
+              {
+                "title": "蓝",
+                "awakening": false,
+                "content": "A\nA1\nG2\nG3\nC2\nD1\nD2\n等蓝条50了A"
+              }
+            ]
+          }
+        ],
+        "currentBranch": 0
+      },
+      {
+        "branches": [
+          {
+            "name": "蓝条50后",
+            "items": [
+              {
+                "title": "T10",
+                "awakening": true,
+                "content": "开奥义直接打"
+              },
+              {
+                "title": "T11",
+                "awakening": false,
+                "content": "B\nF1>H\nH1\nH3\nI1\nI3"
+              },
+              {
+                "title": "T12",
+                "awakening": true,
+                "content": "开奥义直接打"
+              }
+            ]
+          }
+        ],
+        "currentBranch": 0
+      },
+      {
+        "branches": [
+          {
+            "name": "5驱散+FC",
+            "items": [
+              {
+                "title": "5驱散+FC特动",
+                "awakening": false,
+                "content": "A出去解3层\nH2\nI2\nFC\n等待所有光背结束。\n后续驱散无技能时可使用\nE"
+              }
+            ]
+          },
+          {
+            "name": "其他特动",
+            "items": [
+              {
+                "title": "其他特动",
+                "awakening": false,
+                "content": "C\n等待所有光背结束"
+              }
+            ]
+          }
+        ],
+        "currentBranch": 0
+      },
+      {
+        "branches": [
+          {
+            "name": "光背消失",
+            "items": [
+              {
+                "title": "无特动",
+                "awakening": false,
+                "content": "能蹭神盾就蹭，为了100做准备"
+              },
+              {
+                "title": "遇到特动",
+                "awakening": true,
+                "content": "F3\n或\nD\n或\nH3\n（主角和牛蹲）"
+              }
+            ]
+          }
+        ],
+        "currentBranch": 0
+      },
+      {
+        "branches": [
+          {
+            "name": "蓝条100后",
+            "items": [
+              {
+                "title": "蓝条100后",
+                "awakening": false,
+                "content": "D\n或\nF3\n或 DC右技能"
+              },
+              {
+                "title": "特动120hit",
+                "awakening": false,
+                "content": "H1"
+              },
+              {
+                "title": "600w 5次",
+                "awakening": false,
+                "content": "C\nFC"
+              },
+              {
+                "title": "6驱散",
+                "awakening": false,
+                "content": "E"
+              }
+            ]
+          }
+        ],
+        "currentBranch": 0
+      }
+    ],
+    "settings": {
+      "roles": [
+        {
+          "roleImage": "100580_sw_1_01",
+          "skills": [
+            "2711_3",
+            "188_3",
+            "2712_3",
+            "2709_1"
+          ]
+        },
+        {
+          "roleImage": "3040222000_01",
+          "skills": [
+            "56_3",
+            "911_1",
+            "912_3",
+            ""
+          ]
+        },
+        {
+          "roleImage": "3040456000_02",
+          "skills": [
+            "1993_3",
+            "1994_1",
+            "1995_3",
+            ""
+          ]
+        },
+        {
+          "roleImage": "3040376000_01",
+          "skills": [
+            "1637_3",
+            "1638_1",
+            "1639_5",
+            ""
+          ]
+        },
+        {
+          "roleImage": "3040169000_03",
+          "skills": [
+            "801_5",
+            "802_3",
+            "803_3",
+            "2046_3"
+          ]
+        },
+        {
+          "roleImage": "3040265000_01",
+          "skills": [
+            "1109_3",
+            "1110_3",
+            "1111_4",
+            "1112_3"
+          ]
+        },
+        {
+          "roleImage": "3040443000_01",
+          "skills": [
+            "1925_4",
+            "1926_4",
+            "1927_4",
+            ""
+          ]
+        },
+        {
+          "roleImage": "3040610000_01_1",
+          "skills": [
+            "2692_3",
+            "2693_1",
+            "2691_3",
+            ""
+          ]
+        },
+        {
+          "roleImage": "3040536000_01_1",
+          "skills": [
+            "2364_3",
+            "2365_1",
+            "2366_3",
+            ""
+          ]
+        }
+      ],
+      "stones": [
+        {
+          "imageUrl": "2040327000"
+        },
+        {
+          "imageUrl": "2040090000_04"
+        },
+        {
+          "imageUrl": "2040425000"
+        },
+        {
+          "imageUrl": "2040205000"
+        },
+        {
+          "imageUrl": "2040408000"
+        }
+      ]
+    }
+  }
+};
