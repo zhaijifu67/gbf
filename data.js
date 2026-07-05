d8fc8223ff8a06482844e1220c4b636a3e23a6b2
宅爷 <744561141@qq.com>
2026-07-05 22:47:00 +0800

    ⭐ 更新轴

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.data = {
-  "暗极妈": {
+  "暗极妈开荒": {
     "rows": [
       {
         "branches": [
@@ -52,17 +52,17 @@ window.data = {
             "name": "90血以下",
             "items": [
               {
-                "title": "t6",
+                "title": "t6 1E伤害",
                 "awakening": false,
                 "content": "B\nA4\nG2\nC1\nC2\nC3"
               },
               {
-                "title": "T7",
+                "title": "T7 奥义6次",
                 "awakening": true,
                 "content": "开奥义直接打"
               },
               {
-                "title": "T8",
+                "title": "T8 FC+80hit",
                 "awakening": false,
                 "content": "FC\n等待70血以下A"
               }
@@ -77,7 +77,7 @@ window.data = {
             "name": "红",
             "items": [
               {
-                "title": "红",
+                "title": "红 5奥义",
                 "awakening": true,
                 "content": "A\nA1\nD1\n等蓝条50了A"
               }
@@ -87,7 +87,7 @@ window.data = {
             "name": "蓝",
             "items": [
               {
-                "title": "蓝",
+                "title": "蓝 6技能",
                 "awakening": false,
                 "content": "A\nA1\nG2\nG3\nC2\nD1\nD2\n等蓝条50了A"
               }
@@ -104,12 +104,12 @@ window.data = {
               {
                 "title": "T10",
                 "awakening": true,
-                "content": "开奥义直接打"
+                "content": "开奥义，除了主角本T都会死，在本T可以把技能都开了贪伤害"
               },
               {
                 "title": "T11",
                 "awakening": false,
-                "content": "B\nF1>H\nH1\nH3\nI1\nI3"
+                "content": "换人上场\nB\nF1>H\nH1\nH3\nI1\nI3\n要注意boss的状态：\n尾：必须4奥义(比较难解)\n足：必须关奥义\n手：注意血量不要太低会直接死\n其他状态随意"
               },
               {
                 "title": "T12",
@@ -154,12 +154,12 @@ window.data = {
               {
                 "title": "无特动",
                 "awakening": false,
-                "content": "如果此时是切利天，最好提前基队友神盾，否则很容易没技能防御\n没基到神盾且自己没有神盾的话就别动了\n其他情况不开奥义可以直接解除\n注意下面两个技能好了记得放\nI3\nH1"
+                "content": "【作为开荒的话你的任务已经完成了，死了也不要有负担，可以直接等g100再动也完全ok】\n由于切利天(尾)没解除会延迟技能，最好提前基队友神盾，否则很容易没技能防御过特动\n没基到神盾且自己没有神盾的话就别动了\n其他情况不开奥义可以直接解除\n注意下面两个技能好了记得放\nI3\nH1"
               },
               {
                 "title": "遇到特动",
                 "awakening": true,
-                "content": "暗属性解不了除了驱散的特动，可以使用以下方式硬吃\nF3\n使用技能容易被切利天推迟\nH3\n使用牛3需要主角和牛蹲\nD"
+                "content": "暗属性解不了除了驱散的特动，可以使用以下方式硬吃\nF3\n使用技能容易被切利天推迟\nH3\n使用牛3需要主角和牛蹲\nD\n神盾简单粗暴"
               }
             ]
           }
@@ -172,9 +172,9 @@ window.data = {
             "name": "蓝条100后",
             "items": [
               {
-                "title": "蓝条100后",
+                "title": "g100",
                 "awakening": true,
-                "content": "D\n神盾可以提前基\nF3\n或DC右技能，本T可以考虑开奥义续咖喱"
+                "content": "本T大伤害，必须防御过\nD\n神盾可以提前基\nF3\n或零妈3\n或DC右技能(不推荐)"
               },
               {
                 "title": "特动120hit",
@@ -184,12 +184,17 @@ window.data = {
               {
                 "title": "600w 5次",
                 "awakening": false,
-                "content": "解不掉，需要DC左技能\nC\nFC"
+                "content": "解不掉，需要DC左技能\n如果用完DC还剩2次未解除可以用000的双奥主角打奥义来解\nC"
               },
               {
                 "title": "6驱散",
                 "awakening": false,
-                "content": "E"
+                "content": "H2\nI2\n不开奥义也有驱散"
+              },
+              {
+                "title": "15弱体",
+                "awakening": true,
+                "content": "I3>H\nI2\nH1\nH1\nH2\nH2\n零妈开奥义可以帮忙解，也可以开BB帮忙解\nE"
               }
             ]
           }
@@ -296,6 +301,318 @@ window.data = {
         },
         {
           "imageUrl": "2040408000"
+        },
+        {
+          "imageUrl": "2040327000"
+        }
+      ]
+    }
+  },
+  "暗极妈-妈武妈召": {
+    "rows": [
+      {
+        "branches": [
+          {
+            "name": "火",
+            "items": [
+              {
+                "title": "T1",
+                "awakening": true,
+                "content": "F\nE4\nE2>A\nE3>A\nC3\nC2\n"
+              },
+              {
+                "title": "T2",
+                "awakening": true,
+                "content": "主角开\nA1\nA3"
+              },
+              {
+                "title": "T3",
+                "awakening": true,
+                "content": "E1\nA4\nB2"
+              },
+              {
+                "title": "T4",
+                "awakening": true,
+                "content": "F1>A\n保证主角奥义60/70\nA出去火解除"
+              }
+            ]
+          }
+        ],
+        "currentBranch": 0
+      },
+      {
+        "branches": [
+          {
+            "name": "火解除~90",
+            "items": [
+              {
+                "title": "T5",
+                "awakening": false,
+                "content": "A2\nB1\nC1\nC2\nBP\n等所有火解掉了A"
+              },
+              {
+                "title": "T6",
+                "awakening": false,
+                "content": "F1\nF2>G\nC\nG2\nC3\nB4\n等90血A"
+              }
+            ]
+          }
+        ],
+        "currentBranch": 0
+      },
+      {
+        "branches": [
+          {
+            "name": "90",
+            "items": [
+              {
+                "title": "T7 1E伤害",
+                "awakening": false,
+                "content": "A1\nA\n"
+              },
+              {
+                "title": "T8 奥义6次",
+                "awakening": true,
+                "content": "主角关\nA3"
+              }
+            ]
+          }
+        ],
+        "currentBranch": 0
+      },
+      {
+        "branches": [
+          {
+            "name": "蓝",
+            "items": [
+              {
+                "title": "T9 FC+80hit",
+                "awakening": false,
+                "content": "FC\n主角开\nA3\n等待70再A"
+              },
+              {
+                "title": "T10 70血后 6技能",
+                "awakening": true,
+                "content": "技能全开偷伤害\n等待蓝条50开奥义A"
+              }
+            ]
+          },
+          {
+            "name": "红",
+            "items": [
+              {
+                "title": "T9 FC+80hit",
+                "awakening": false,
+                "content": "FC\nG3"
+              },
+              {
+                "title": "T10 TA+DB",
+                "awakening": false,
+                "content": "【弱体特动】把弱体技能开掉（可以全开），等待70血A\n【奥义特动】技能全开开奥义，下方的70血不动直接G50"
+              },
+              {
+                "title": "T11 70血后",
+                "awakening": true,
+                "content": "【t10弱体特动】等蓝条50开奥义打"
+              }
+            ]
+          }
+        ],
+        "currentBranch": 0
+      },
+      {
+        "branches": [
+          {
+            "name": "g50特动",
+            "items": [
+              {
+                "title": "g50",
+                "awakening": true,
+                "content": "自由发挥\n等所有人发hi了A"
+              }
+            ]
+          }
+        ],
+        "currentBranch": 0
+      },
+      {
+        "branches": [
+          {
+            "name": "换人~g100",
+            "items": [
+              {
+                "title": "+1t自由发挥",
+                "awakening": false,
+                "content": "F\nH1\nH3\nI1\nI3\nF1>H\n【根据debuff开关奥义】\n脚必须关奥义\n环必须开奥义\n眼睛/角/手随意，手注意补充血量\n【特殊情况】+1t遇到4奥义(环)如果没充能石直接摆烂\n其他t遇到4奥义(环)注意关主角和零妈开奥义打"
+              },
+              {
+                "title": "+2t自由发挥",
+                "awakening": false,
+                "content": "可以考虑本T使用充能石充能，存奥义应对四奥义"
+              },
+              {
+                "title": "满豆特动 FC+驱散",
+                "awakening": false,
+                "content": "FC\nH2\nI3\n只有此特动可以解除，如果遇到尽量解掉，解不掉有2T强压注意先回血再防打"
+              },
+              {
+                "title": "满豆特动 其他",
+                "awakening": false,
+                "content": "第一次满豆用000解除光背\nC\n第二次用零妈硬吃\nF3\n第三次神盾硬吃\nE\n【注意】在g100前尽量暖好零妈4，尽量，非必须\nF4"
+              },
+              {
+                "title": "g100",
+                "awakening": false,
+                "content": "【本T岛15血量会锁血，可以关奥义存一下】\n用零妈硬吃\nF3\n或用神盾硬吃\nE\n都没有了可以用麒麟刷零妈技能（最好能基一个神盾就不用用了，零妈留给100）\nD\n或用牛技能+全蹲（能进但是100后容易死）\nH3"
+              }
+            ]
+          }
+        ],
+        "currentBranch": 0
+      },
+      {
+        "branches": [
+          {
+            "name": "g100后",
+            "items": [
+              {
+                "title": "【注意】",
+                "awakening": false,
+                "content": "不管什么特动尽量开一下牛，否则容易暴毙\nH3"
+              },
+              {
+                "title": "120hit",
+                "awakening": false,
+                "content": "A"
+              },
+              {
+                "title": "600",
+                "awakening": true,
+                "content": "用DC左技能解掉并且回FC\n如果DC后剩余两次可以考虑000给主角双奥\nC"
+              },
+              {
+                "title": "1E",
+                "awakening": false,
+                "content": "A出去直接解\n（没有妈召buff将跟世界告别）"
+              },
+              {
+                "title": "驱散",
+                "awakening": false,
+                "content": "D\n关奥义有4驱散"
+              },
+              {
+                "title": "15DB",
+                "awakening": false,
+                "content": "I3>H\nI2\nH1\nH1\nH2\nH2\n零妈满奥义可以帮忙解，注意关主角和零妈1，看剩余次数可以BB\nB"
+              }
+            ]
+          }
+        ],
+        "currentBranch": 0
+      }
+    ],
+    "settings": {
+      "roles": [
+        {
+          "roleImage": "100580_sw_1_01",
+          "skills": [
+            "2711_3",
+            "2712_3",
+            "2708_3",
+            "2709_1"
+          ]
+        },
+        {
+          "roleImage": "3040035000_04",
+          "skills": [
+            "1025_3",
+            "1459_1",
+            "1009_1",
+            "365_3"
+          ]
+        },
+        {
+          "roleImage": "3040628000_01",
+          "skills": [
+            "2771_1",
+            "2772_1",
+            "2773_3",
+            ""
+          ]
+        },
+        {
+          "roleImage": "3040337000_01",
+          "skills": [
+            "",
+            "",
+            "",
+            ""
+          ]
+        },
+        {
+          "roleImage": "3040169000_03",
+          "skills": [
+            "801_5",
+            "802_3",
+            "803_3",
+            "2046_3"
+          ]
+        },
+        {
+          "roleImage": "3040265000_01",
+          "skills": [
+            "1109_3",
+            "1110_3",
+            "1111_4",
+            "1112_3"
+          ]
+        },
+        {
+          "roleImage": "3040611000_01",
+          "skills": [
+            "2696_1",
+            "2697_3",
+            "2698_3",
+            ""
+          ]
+        },
+        {
+          "roleImage": "3040610000_01_1",
+          "skills": [
+            "2692_3",
+            "2693_1",
+            "2691_3",
+            ""
+          ]
+        },
+        {
+          "roleImage": "3040536000_01_1",
+          "skills": [
+            "2364_3",
+            "2365_1",
+            "2366_3",
+            ""
+          ]
+        }
+      ],
+      "stones": [
+        {
+          "imageUrl": "2040448000"
+        },
+        {
+          "imageUrl": "2040408000"
+        },
+        {
+          "imageUrl": "2040425000"
+        },
+        {
+          "imageUrl": "2040158000"
+        },
+        {
+          "imageUrl": "2040205000"
+        },
+        {
+          "imageUrl": "2040090000_04"
         }
       ]
     }
